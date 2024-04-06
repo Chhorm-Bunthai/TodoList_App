320594234764037f1809239340f4ce7665d6dcd3
Chhorm Bunthai <bunthaicambo89@gmail.com>
2024-04-06 22:02:26 +0700

    change name of file from lock to todolist

--- server/scripts/Deploy.js
+++ server/scripts/Deploy.js
@@ -4,7 +4,7 @@ const { ethers } = require("hardhat");
 async function main() {
   const TodoList = await ethers.getContractFactory("TodoList");
   const todoList = await TodoList.deploy();
-  console.log(`TodoList deployed to:11 ${todoList.target}`);
+  console.log(`TodoList deployed to: ${todoList.target}`);
 }
 
 main().catch((error) => {
