cf80de8b64733e97e5d6ace02edfd7ef93035a5d
Chhorm Bunthai <bunthaicambo89@gmail.com>
2024-04-07 21:53:58 +0700

    adding MUI library

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,74 +1,56 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { ethers } from "ethers";
 import { taskContractAbi, taskContractAddress } from "./utils/config";
+import {
+  TextField,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  Container,
+} from "@mui/material";
 
-function App() {
+function useBlockchainData() {
   const [taskCount, setTaskCount] = useState(0);
   const [tasks, setTasks] = useState([]);
-  const [newTaskContent, setNewTaskContent] = useState("");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const initializeProvider = () =>
-    new ethers.providers.Web3Provider(window.ethereum);
-
-  const fetchTasks = async (provider) => {
-    const signer = provider.getSigner();
-    const todoListContract = new ethers.Contract(
-      taskContractAddress,
-      taskContractAbi,
-      signer
-    );
-    const count = await todoListContract.taskCount();
-    setTaskCount(count.toNumber());
-    const loadedTasks = [];
-    for (let i = 1; i <= count; i++) {
-      const task = await todoListContract.tasks(i);
-      loadedTasks.push({
-        id: task.id.toNumber(),
-        content: task.content,
-        completed: task.completed,
-      });
-    }
-    setTasks(loadedTasks);
-  };
-
   useEffect(() => {
+    const fetchTasks = async (provider) => {
+      try {
+        const signer = provider.getSigner();
+        const todoListContract = new ethers.Contract(
+          taskContractAddress,
+          taskContractAbi,
+          signer
+        );
+        const count = await todoListContract.taskCount();
+        setTaskCount(count.toNumber());
+        const loadedTasks = [];
+        for (let i = 1; i <= count; i++) {
+          const task = await todoListContract.tasks(i);
+          loadedTasks.push({
+            id: task.id.toNumber(),
+            content: task.content,
+            completed: task.completed,
+          });
+        }
+        setTasks(loadedTasks);
+      } catch (error) {
+        setError("Error loading blockchain data: " + error.message);
+      }
+    };
+
     const loadBlockchainData = async () => {
       try {
         if (window.ethereum && window.ethereum.isMetaMask) {
           await window.ethereum.request({ method: "eth_requestAccounts" });
-          const provider = initializeProvider();
-          const todoListContract = new ethers.Contract(
-            taskContractAddress,
-            taskContractAbi,
-            provider
-          );
+          const provider = new ethers.providers.Web3Provider(window.ethereum);
           await fetchTasks(provider);
-
-          // Event listeners for TaskCreated and TaskCompleted events
-          const handleTaskCreated = async (
-            taskId,
-            content,
-            completed,
-            event
-          ) => {
-            await fetchTasks(provider);
-          };
-          const handleTaskCompleted = async (taskId, completed, event) => {
-            await fetchTasks(provider);
-          };
-          todoListContract.on("TaskCreated", handleTaskCreated);
-          todoListContract.on("TaskCompleted", handleTaskCompleted);
-
-          // Cleanup function to remove event listeners when component unmounts
-          return () => {
-            todoListContract.removeListener("TaskCreated", handleTaskCreated);
-            todoListContract.removeListener(
-              "TaskCompleted",
-              handleTaskCompleted
-            );
-          };
         } else {
           setError("MetaMask not detected or not enabled!");
         }
@@ -78,13 +60,23 @@ function App() {
     };
 
     loadBlockchainData();
+
+    return () => {}; // Cleanup function
   }, []);
+
+  return { taskCount, tasks, loading, error };
+}
+
+function App() {
+  const [newTaskContent, setNewTaskContent] = useState("");
+
+  const { taskCount, tasks, loading, error } = useBlockchainData();
 
   const handleCreateTask = async () => {
     if (!newTaskContent) return;
     try {
       setLoading(true);
-      const provider = initializeProvider();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
       await window.ethereum.request({ method: "eth_requestAccounts" });
       const signer = provider.getSigner();
       const todoListContract = new ethers.Contract(
@@ -94,7 +86,6 @@ function App() {
       );
       await todoListContract.createTask(newTaskContent);
       setNewTaskContent("");
-      await fetchTasks(provider);
     } catch (error) {
       setError("Error creating task: " + error.message);
     } finally {
@@ -105,7 +96,7 @@ function App() {
   const handleToggleCompleted = async (taskId) => {
     try {
       setLoading(true);
-      const provider = initializeProvider();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
       await window.ethereum.request({ method: "eth_requestAccounts" });
       const signer = provider.getSigner();
       const todoListContract = new ethers.Contract(
@@ -114,7 +105,6 @@ function App() {
         signer
       );
       await todoListContract.toggleCompleted(taskId);
-      await fetchTasks(provider);
     } catch (error) {
       setError("Error toggling task completion: " + error.message);
     } finally {
@@ -123,34 +113,77 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <h1>Todo List</h1>
+    <Container
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <Typography variant="h1" component="h1">
+        Todo List
+      </Typography>
       {error && <div className="error">{error}</div>}
-      <div>
-        <input
-          type="text"
-          placeholder="Enter new task"
-          value={newTaskContent}
-          onChange={(e) => setNewTaskContent(e.target.value)}
-        />
-        <button onClick={handleCreateTask} disabled={loading}>
-          {loading ? "Adding Task..." : "Add Task"}
-        </button>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+        }}
+      >
+        <div
+          style={{ margin: "20px 0", display: "flex", alignItems: "center" }}
+        >
+          <TextField
+            type="text"
+            placeholder="Enter new task"
+            value={newTaskContent}
+            onChange={(e) => setNewTaskContent(e.target.value)}
+            style={{ marginRight: "10px", flex: 1 }}
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleCreateTask}
+            disabled={loading}
+            style={{ width: "120px" }}
+          >
+            {loading ? "Adding Task..." : "Add Task"}
+          </Button>
+        </div>
+        <div>
+          <List>
+            {tasks.map((task) => (
+              <ListItem key={task.id} disablePadding>
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        checked={task.completed}
+                        onChange={() => handleToggleCompleted(task.id)}
+                        disabled={loading}
+                      />
+                    }
+                    label={
+                      <ListItemText
+                        primary={task.content}
+                        style={{
+                          textDecoration: task.completed
+                            ? "line-through"
+                            : "none",
+                        }}
+                      />
+                    }
+                  />
+                </div>
+              </ListItem>
+            ))}
+          </List>
+        </div>
       </div>
-      <ul>
-        {tasks.map((task) => (
-          <li key={task.id}>
-            <input
-              type="checkbox"
-              checked={task.completed}
-              onChange={() => handleToggleCompleted(task.id)}
-              disabled={loading}
-            />
-            <span>{task.content}</span>
-          </li>
-        ))}
-      </ul>
-    </div>
+    </Container>
   );
 }
 
